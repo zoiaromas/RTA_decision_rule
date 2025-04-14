6db1390785e7a9267f971a5c0ca20fb43b329a2e
Zoia Romas <romas.zoia@gmail.com>
2025-04-14 14:26:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,3 @@ def not_found(error):
 @app.errorhandler(400)
 def bad_request(error):
     return jsonify({"error": "Bad Request"}), 400
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
